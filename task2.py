b75e4a922c8a606ac174b113d6ec504f80735b48
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2025-02-14 20:31:49 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,14 @@
-
+def typeBasedTransformer(**kwargs):
+  newkwargs={}
+  for key, value in kwargs.items():
+    if isinstance(value, bool):
+      newkwargs[key]=not(value)
+    elif isinstance(value, int) or isinstance(value, float):
+      newkwargs[key]=value**2
+    elif isinstance(value, str):
+      newkwargs[key]=value[::-1]
+    elif isinstance(value, list) or isinstance(value, tuple):
+      newkwargs[key]=value[::-1]
+    elif isinstance(value, dict):
+      newkwargs[key]={val: new_key for new_key, val in value.items()}
+  return newkwargs
